1aa98dc657711890f1550474363188336f9908ef
luizfernandowitt <luizfernandowitt00@gmail.com>
2023-03-04 18:22:35 -0300

    feat: ensure AxiosHttpClientAdapter returns correct values

---
+++ src/infra/http/axios-http-client/axios-http-client-adapter.ts
@@ -0,0 +1,13 @@
+import { type HttpResponse, type HttpPostParams, type HttpPostClient } from 'data/protocols/http'
+import axios from 'axios'
+
+export class AxiosHttpClientAdapter implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
+  }
+}
