f4d008ca1458ec0deee19221ef2515722938a24e
luizfernandowitt <luizfernandowitt00@gmail.com>
2023-03-04 20:03:17 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts'
   ],
+  moduleNameMapper: {
+    '\\.scss$': 'identity-obj-proxy'
+  },
   modulePaths: ['node_modules', '<rootDir>/src'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
